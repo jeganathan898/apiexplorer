2818c02a49bd51a138ca69b18173bb6a2ca78f3b
Prithiviraj1397 <prithiviraj@colanonline.com>
2023-11-01 11:57:28 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 
-function App(){
+function App() {
   
   return (
     <Router>
